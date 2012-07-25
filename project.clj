584ab503801f03fc16aeaf39bb5ff31cd66c7021
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2012-07-25 23:02:59 +0900

    update :description and :url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-datomic-session "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Datomic version of Ring's http session storage"
+  :url "https://github.com/hozumi/clj-datomic-session"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
